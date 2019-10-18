5e363765e4465534cb32f19eae1e8349ae41ea0c
HOMEOFFICE\vn0zsmq <anuraj.sivankutty@walmart.com>
2019-10-18 22:35:15 +0530

    Hibernate id generator with UUID example

---
+++ hibernate-uuid-generator-example/src/me/anuraj/Employee.java
@@ -0,0 +1,26 @@
+package me.anuraj;
+
+public class Employee {  
+	private String id;  
+	private String firstName,lastName;  
+
+	public String getId() {  
+		return id;  
+	}  
+	public void setId(String string) {  
+		this.id = string;  
+	}  
+	public String getFirstName() {  
+		return firstName;  
+	}  
+	public void setFirstName(String firstName) {  
+		this.firstName = firstName;  
+	}  
+	public String getLastName() {  
+		return lastName;  
+	}  
+	public void setLastName(String lastName) {  
+		this.lastName = lastName;  
+	}  
+
+}  
