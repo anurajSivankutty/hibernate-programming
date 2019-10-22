8719b48e15a5fab957c4aa2a0b4039960ce99d8d
HOMEOFFICE\vn0zsmq <anuraj.sivankutty@walmart.com>
2019-10-22 23:23:23 +0530

    Hibernate table per concrete class mapping strategy

---
+++ hibernate-inheritance-mapping-table-per-concrete-class/src/me/anuraj/Employee.java
@@ -0,0 +1,42 @@
+package me.anuraj;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+public class Employee {  
+	private String id;  
+	private String firstName,lastName;  
+
+	public String getId() {  
+		return id;  
+	}  
+	public void setId(String string) {  
+		this.id = string;  
+	}  
+	public String getFirstName() {  
+		return firstName;  
+	}  
+	public void setFirstName(String firstName) {  
+		this.firstName = firstName;  
+	}  
+	public String getLastName() {  
+		return lastName;  
+	}  
+	public void setLastName(String lastName) {  
+		this.lastName = lastName;  
+	}  
+
+	@Override
+	public String toString() {
+		return "["+this.getClass().getName()+"] Name:" + this.getFirstName() + " " + this.getLastName();
+	}
+}  
---
+++ hibernate-inheritance-mapping-table-per-concrete-class/src/me/anuraj/RegularEmployee.java
@@ -0,0 +1,24 @@
+package me.anuraj;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+
+public class RegularEmployee extends Employee{  
+	private float salary;
+	private float bonus;
+	
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	public float getBonus() {
+		return bonus;
+	}
+	public void setBonus(float bonus) {
+		this.bonus = bonus;
+	}
+	
+}  
