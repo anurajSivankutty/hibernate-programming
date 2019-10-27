411368c1ce21a49fcce5e04ad8c3f56b05337dc0
HOMEOFFICE\vn0zsmq <anuraj.sivankutty@walmart.com>
2019-10-28 01:38:30 +0530

    table per subclass using xml configuration

---
+++ hibernate-inheritance-mapping-table-per-subclass-using-xml-example/src/me/anuraj/ContractEmployee.java
@@ -0,0 +1,20 @@
+package me.anuraj;
+
+ 
+public class ContractEmployee extends Employee{
+	private float payPerHour;
+	private int contractDuration;
+	public float getPayPerHour() {
+		return payPerHour;
+	}
+	public void setPayPerHour(float payPerHour) {
+		this.payPerHour = payPerHour;
+	}
+	public int getContractDuration() {
+		return contractDuration;
+	}
+	public void setContractDuration(int contractDuration) {
+		this.contractDuration = contractDuration;
+	}
+	
+}
---
+++ hibernate-inheritance-mapping-table-per-subclass-using-xml-example/src/me/anuraj/Employee.java
@@ -0,0 +1,32 @@
+package me.anuraj;
+
+public class Employee {  
+	private String id;  
+
+	private String firstName;
+	private String lastName;  
+
+	public String getId() {  
+		return id;  
+	}  
+	public void setId(String string) {  
+		this.id = string;  
+	}  
+	public String getFirstName() {  
+		return firstName;  
+	}  
+	public void setFirstName(String firstName) {  
+		this.firstName = firstName;  
+	}  
+	public String getLastName() {  
+		return lastName;  
+	}  
+	public void setLastName(String lastName) {  
+		this.lastName = lastName;  
+	}  
+
+	@Override
+	public String toString() {
+		return "["+this.getClass().getName()+"] Name:" + this.getFirstName() + " " + this.getLastName();
+	}
+}  
